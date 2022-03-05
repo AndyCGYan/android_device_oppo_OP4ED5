d53d904e0bc474b11144ee06b504daf0e463a1f0
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2022-03-05 02:45:32 +0000

    OP4ED5: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: oppo Model: PEQM00 Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ omni_OP4ED5.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from OP4ED5 device
+$(call inherit-product, device/oppo/OP4ED5/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := OP4ED5
+PRODUCT_NAME := omni_OP4ED5
+PRODUCT_BRAND := OPPO
+PRODUCT_MODEL := PEQM00
+PRODUCT_MANUFACTURER := oppo
